3ed984db4f5318af9bcce6bdd5d48827b0f2ba26
Hassan Jalali <mister.developer@yahoo.com>
2023-02-17 18:17:50 +0330

    first method added to the userhub class

--- SignalRSample/SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/SignalRSample/Hubs/UserHub.cs
@@ -2,7 +2,17 @@
 
 namespace SignalRSample.Hubs
 {
+    //count the number of views on a web page
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+
+        // called whenever page loaded or reload
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        }
+
     }
 }
